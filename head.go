64b7d876cf904599219743a32f154a5d5bbc4b7e
Jean Silva <Jean.Silva@trivago.com>
2020-05-03 13:55:22 +0200

    Finalizing the `head` command
    It accepts max 2 arguments.
    1) -n Int to the number of lines to read from the beginning of a file or
    stdin.
    2) -f file name if passed, otherwise reads from stdin.

--- head.go
+++ head.go
@@ -1,55 +1,46 @@
 package main
 
-import(
-  "fmt"
-  "os"
-  "bufio"
-  "io"
-  "flag"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
-  var nLines = flag.Int("n", 10, "the number of lines to read")
-  flag.Parse()
+	var nLines = flag.Int("n", 10, "the number of lines to read")
+	var fileName = flag.String("f", "", "filename to read")
+	flag.Parse()
 
-  fmt.Println("n:", *nLines)
-  fmt.Println("tail:", flag.Args())
-  fmt.Println("Length:", len(flag.Args()))
-  fmt.Println("NArg:", flag.NArg())
+	file := getReader(fileName)
 
-  file := getReader()
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var lines []string
 
-  scanner := bufio.NewScanner(file)
-  scanner.Split(bufio.ScanLines)
-  var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
 
-  for scanner.Scan() {
-    lines = append(lines, scanner.Text())
-  }
-
-  for i := 0; i < *nLines; i++ {
-    fmt.Println(lines[i])
-  }
+	for i := 0; i < len(lines); i++ {
+		if i == *nLines {
+			break
+		}
+		fmt.Println(lines[i])
+	}
 }
 
-func getReader() io.Reader {
-  var err error
-  r := os.Stdin
+func getReader(fileName *string) io.Reader {
+	r := os.Stdin
+	var err error
 
-  fileName := ""
-  for i := 0; i < len(flag.Args()); i++ {
-    if _, err := os.Stat(flag.Args()[i]); err == nil {
-      fileName = flag.Args()[i]
-      break
-    }
-  }
+	if *fileName != "" {
+		r, err = os.Open(*fileName)
+		if err != nil {
+			panic(err)
+		}
+	}
 
-  if fileName != "" {
-    r, err = os.Open(fileName)
-    if err != nil {
-      panic(err)
-    }
-  }
-
-  return r
+	return r
 }
