21898953fb41bf4447ceee11cae40254a3421aa9
Jean Silva <Jean.Silva@trivago.com>
2020-05-03 15:18:49 +0200

    Validate the size of the stdin or file
    Also compile for Macos instead of FreeBSD

--- Makefile
+++ Makefile
@@ -6,6 +6,6 @@ run:
 
 compile:
 	echo "Compiling for every OS and Platform"
-	GOOS=freebsd GOARCH=386 go build -o bin/head-freebsd-386 commands/head.go
+	GOOS=darwin GOARCH=amd64 go build -o bin/head commands/head.go
 	GOOS=linux GOARCH=386 go build -o bin/head-linux-386 commands/head.go
 	GOOS=windows GOARCH=386 go build -o bin/head.exe commands/head.go
--- commands/head.go
+++ commands/head.go
@@ -13,34 +13,46 @@ func main() {
 	var fileName = flag.String("f", "", "filename to read")
 	flag.Parse()
 
-	file := getReader(fileName)
+	reader := getReader(fileName)
 
-	scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(reader)
 	scanner.Split(bufio.ScanLines)
-	var lines []string
 
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	for i := 0; i < len(lines); i++ {
-		if i == *nLines {
+	for i := 0; i < *nLines; i++ {
+		if !scanner.Scan() {
 			break
 		}
-		fmt.Println(lines[i])
+		fmt.Println(scanner.Text())
 	}
 }
 
 func getReader(fileName *string) io.Reader {
-	r := os.Stdin
-	var err error
-
 	if *fileName != "" {
-		r, err = os.Open(*fileName)
+		f, err := os.Open(*fileName)
 		if err != nil {
 			panic(err)
 		}
+
+		validateInput(f)
+
+		return f
 	}
 
-	return r
+	f := os.Stdin
+	validateInput(f)
+
+	return f
+}
+
+func validateInput(f *os.File) {
+	fi, err := f.Stat()
+	if err != nil {
+		fmt.Println("Error stat()")
+		os.Exit(1)
+	}
+
+	if fi.Size() <= 0 {
+		fmt.Println("input has no length")
+		os.Exit(1)
+	}
 }
